1a123ea4a27890d6b3f839b60d3fd3e83f6d55bb
GitSid2k <78859731+GitSid2k@users.noreply.github.com>
2021-02-27 18:41:22 +0300

    Add files via upload

---
+++ Cat_class.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, male, age):
+        self.name = name
+        self.male = male
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_sex(self):
+        return self.male
+
+    def get_age(self):
+        return self.age
---
+++ Cat_object.py
@@ -0,0 +1,14 @@
+from Cat_class import Cat
+
+Cat_1 = Cat("Барон", "Мальчик", "2 Года")
+print("Имя:", Cat_1.name)
+print("Пол:", Cat_1.male)
+print("Возраст:", Cat_1.age)
+
+print()
+
+Cat_2 = Cat("Сэм", "Мальчик", "2 Года")
+print("Имя:", Cat_2.name)
+print("Пол:", Cat_2.male)
+print("Возраст:", Cat_2.age)
+
